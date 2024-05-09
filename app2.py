794657d7496124c9f6c273c5e3762bfc8483cb1e
marcosricciardi12 <marcosricciardi12@gmail.com>
2024-05-09 02:42:59 -0300

    data

--- app2.py
+++ app2.py
@@ -12,7 +12,7 @@ temp_f = 80
 delta_temp_total = temp_f - temp_i
 delta_temp_1s = 0.12
 tiempo_trabajo_incial = 0
-tiempo_trabajo_final = 700
+tiempo_trabajo_final = 700 #600
 calor_perdido = 9.21
 tick_tiempo = 1
 
@@ -24,6 +24,7 @@ x = np.arange(tiempo_trabajo_incial,tiempo_trabajo_final, tick_tiempo)
 # plt.title('Temperatura/Tiempo')
 # plt.show()
 
+#no 10002 si no 602.7845
 y = np.empty(0, dtype=float)
 y = np.append(y, 8.0)
 for index in range(1, len(x)):
