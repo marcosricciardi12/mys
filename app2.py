b1cb2694c3fad14e4ddf5609be8bb94107f88a19
marcos <marcosricardi12@gmail.com>
2024-05-23 10:12:14 -0300

    update

--- app2.py
+++ app2.py
@@ -12,21 +12,27 @@ temp_f = 80
 delta_temp_total = temp_f - temp_i
 delta_temp_1s = 0.12
 tiempo_trabajo_incial = 0
-tiempo_trabajo_final = 700 #600
-calor_perdido = 9.21
+tiempo_trabajo_final = 600 #600
+calor_perdido = 1.997
 tick_tiempo = 1
+potencia = 602.7845
 
 x = np.arange(tiempo_trabajo_incial,tiempo_trabajo_final, tick_tiempo)
-# y = delta_temp_total + delta_temp_1s*x
-# plt.plot(x,y)
-# plt.xlabel('Tiempo(Seg)')
-# plt.ylabel('Temperatura (ºC)')
-# plt.title('Temperatura/Tiempo')
-# plt.show()
+y = temp_i + delta_temp_1s*x
+print(x)
+print(y)
+plt.plot(x,y)
+plt.xlabel('Tiempo(Seg)')
+plt.ylabel('Temperatura (ºC)')
+plt.title('Temperatura/Tiempo')
 
-#no 10002 si no 602.7845
-y = np.empty(0, dtype=float)
-y = np.append(y, 8.0)
+
+# no 10002 si no 602.7845
+y1 = np.empty(0, dtype=float)
+y1 = np.append(y1, 8.0)
 for index in range(1, len(x)):
-     y  = np.append(y, y[index-1] + ((1002.784-9.21*y[index-1])/(1.2*4186)))
-     print(y[index])
+     y1  = np.append(y1, y1[index-1] + ((potencia - calor_perdido*y1[index-1])/(1.2*4186)))
+     # print(y1[index])
+
+plt.plot(x,y1)
+plt.show()
